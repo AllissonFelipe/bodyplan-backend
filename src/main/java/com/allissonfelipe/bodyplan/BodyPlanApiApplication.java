6ddd53c10d736bd44cb0dd25d4f8ac75e7249d15
Allisson Felipe <allissonfmbs95@gmail.com>
2026-09-14 08:33:46 -0300

    fix: remove UTF-8 BOM from Java sources so Maven compile succeeds

--- src/main/java/com/allissonfelipe/bodyplan/BodyPlanApiApplication.java
+++ src/main/java/com/allissonfelipe/bodyplan/BodyPlanApiApplication.java
@@ -1,4 +1,4 @@
-﻿package com.allissonfelipe.bodyplan;
+package com.allissonfelipe.bodyplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
